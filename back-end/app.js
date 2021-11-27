dfc5baba4ded1e45acba7e4b61a98d2beaed8fbc
desh-raj-au-17 <fullstackdev318@gmail.com>
2021-11-27 21:54:37 +0530

    changed package.json

--- back-end/app.js
+++ back-end/app.js
@@ -29,7 +29,7 @@ app.use(`/api/v1`, auth);
 app.use(`/api/v1`, order);
 
 if (process.env.NODE_ENV === "PRODUCTION") {
-  app.use(express.static(path.join(__dirname, "../front-end/build")));
+  app.use(express.static(path.resolve(__dirname, "../front-end/build")));
 
   app.get("*", (req, res) => {
     res.sendFile(path.resolve(__dirname, "../front-end/build/index.html"));
