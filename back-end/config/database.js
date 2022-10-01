ecb912b87d7673b7aed4ef6fcc2cadaeebc474db
Desh Raj <84398702+desh-raj-au-17@users.noreply.github.com>
2022-10-01 11:02:11 +0530

    Update database.js

--- back-end/config/database.js
+++ back-end/config/database.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const connectDatabase = () => {
   mongoose.connect(process.env.DB_URL).then((con) => {
     console.log(`mongodb data base conected with host ${con.connection.host}`);
-  });
+  }).catch((error) => console.error(error))
 };
 
 module.exports = connectDatabase;
